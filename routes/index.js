a70c2419bd5a24cd4f12055cbaf0ccd08be8dc01
yuvalkarif3 <yuvalkarif3@gmail.com>
2021-08-17 16:00:27 +0300

    heroku supp

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 const messages = [
   {
     text: "Hi there!",
     user: "Shemtov",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Hello World!",
     user: "Ashtar",
-    added: new Date()
-  }
+    added: new Date(),
+  },
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Message Board' , messages: messages });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
-router.get('/new', function(req,res,next){
-  res.render('form', { title: 'Mini Message Board' , messages: messages });
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Message Board", messages: messages });
 });
 
-router.post('/new',function(req,res,next){
+router.post("/new", function (req, res, next) {
   const messageText = req.body.messages;
   const messageUser = req.body.name;
-  messages.push({text: messageText,user: messageUser, added: new Date() })
-  res.redirect('/');
-})
-
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
+});
 
 module.exports = router;
